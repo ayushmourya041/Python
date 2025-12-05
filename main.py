0f43db05dfd2140121b64390020d620e8f0a9336
ayushmourya041 <ayushmorya2022@gmail.com>
2025-12-05 21:39:46 +0530

    My frist python code

---
+++ main.py
@@ -0,0 +1,29 @@
+'''SheryiansSchool = "student"  #pascal case
+
+sheriyansSchool = "student"  #camel case
+
+sheriyans_school = "student"  #snake case
+
+a = "Elephant Monkey Lion Pecacock"
+
+print (a[0:8:])
+print (a[:15:])
+print (a[9:18:1])
+print (a[16:25:1])'''
+
+
+name = str(input("Enter your name"))
+
+age = int(input("Enyer your age"))
+
+phone = int(input("Enter your mobile number"))
+
+#print("Welcome to python",name, "your age is",age, "and your mobile number is",phone )
+#print(f"Welcom to python {name} your age is {age} and your mobile number is Ayush{phone}")
+
+print ("Welcome to Python",name)
+print("Your age",age)
+print("and your mobile number",phone)
+
+
+
