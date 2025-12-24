806623c24c43403f4b40597b1a09a1a173bde3ae
ayushmourya041 <ayushmorya2022@gmail.com>
2025-12-24 15:51:31 +0530

    atudy notes and hand tracking file

---
+++ gesture_project/hand_tracking.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
+
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(img_rgb)
+
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(
+                img,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+
+    cv2.imshow("Hand Tracking", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,29 +1,89 @@
-'''SheryiansSchool = "student"  #pascal case
+# '''SheryiansSchool = "student"  # pascal case
+# sheriyansSchool = "student"  # camel case
+# sheriyans_school = "student"  # snake case
+# a = "Elephant Monkey Lion Pecacock"
+# print (a[0:8:])
+# print (a[:15:])
+# print (a[9:18:1])
+# print (a[16:25:1])'''
 
-sheriyansSchool = "student"  #camel case
+# '''name = str(input("Enter your name"))
+# age = int(input("Enyer your age"))
+# phone = int(input("Enter your mobile number"))
+# print("Welcome to Python", name)
+# print("Your age", age)
+# print("and your mobile number", phone)'''
 
-sheriyans_school = "student"  #snake case
+# Conditional statements
+# There are 3 type of statements
+# 1.if 2.if-else 3.if-elif-else
 
-a = "Elephant Monkey Lion Pecacock"
+# '''example1
+# a = 13
+# if a > 10:
+#    print("I will task A")
+# else:
+#    print("I will task B")'''
 
-print (a[0:8:])
-print (a[:15:])
-print (a[9:18:1])
-print (a[16:25:1])'''
+# '''money = int(input("please provide the money;-"))
+# if money == 10:
+#     print("I will have a choco bar icecream")
+# elif money == 20:
+#     print("I will have a frooty")
+# elif money == 30:
+#     print("I will have a cone")
+# else:
+#     print("I will have a vanilla icecream")'''
+
+# '''num1 = int(input("Please tell your first number"))
+# num2 = int(input("Please tell your second number"))
+# if num1 > num2:
+#     print(f"{num1} is greater than {num2}")
+# elif num2 > num1:
+#     print(f"{num2} is greater than {num1}")
+# else:
+#     print("Both the number are same")'''
 
 
-name = str(input("Enter your name"))
+#Que2. Accept the gender from the user as char and print the respective greeting message.
+# '''gen = input("please enter your gender as character (M or F)")
+# if gen == "Male" or gen == "male" or gen == "M" or gen == "m":
+#     print("Good morning SIR")
+# elif gen == "Female" or gen == "female" or gen == "F" or gen == "f":
+#     print("Good morning MAM")
+# else:
+#     print("Unidentified gender")'''
 
-age = int(input("Enyer your age"))
 
-phone = int(input("Enter your mobile number"))
+'''#Que3. Accept an integer and check whether it is an even number or odd.
+num = int(input("Please tell me your number: "))
+if num % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")'''
 
-#print("Welcome to python",name, "your age is",age, "and your mobile number is",phone )
-#print(f"Welcom to python {name} your age is {age} and your mobile number is Ayush{phone}")
 
-print ("Welcome to Python",name)
-print("Your age",age)
-print("and your mobile number",phone)
+#Que.4 Accept name and age from the user. Check if the user is a valid voter or not. 
 
-print(ayush)
+'''name = input("What is your name")
+age = int(input("Enter your age"))
 
+if age >= 18:
+    print (f"Hello {name} you are a valid voter")  
+else:
+    year_age = 18 - age
+    print (f"Hello {name} you are not a valid voter")
+    print (f"You will become a valid voter after {year_age} years")'''
+
+
+#Que5. Accept a year and check if it a year or not (google to find out what is a leap year)
+    
+'''year = int(input("Enter a year: "))
+if year % 100 == 0 and year % 400 == 0:   #this is for century year
+    print(f"{year} is a leap year")
+elif year % 4 == 0 and year % 100 != 0:  #this is for non century year 
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")'''
+
+    
